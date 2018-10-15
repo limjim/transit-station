fe9dd9c93a6604a49f7bcddccfffe1b6c433db49
Kim Tung <nguyenkim08@gmail.com>
2018-10-15 17:11:19 +0700

    version get method

--- src/Controllers/TransformerController.php
+++ src/Controllers/TransformerController.php
@@ -7,42 +7,51 @@ use Megaads\TransitStation\Controllers\Controller;
 
 class TransformerController extends Controller {
 
-    public function transferring(Request $request) {
+    public function transferringRequest(Request $request) {
+        $response = [
+            'status' => 'fail',
+            'message' => 'Request failed.'
+        ];
         header('Content-Type: application/json');
         $url = $request->has('url') ? $request->input('url') : '';
         $header = $request->has('header') ? $request->input('header') : '';
         $method = $request->has('method') ? $request->input('method') : 'GET';
         $param = $request->has('param') ? $request->input('param'): '';
-        if ($param != '' && is_array($param)) {
+        if ($param && !is_array($param)) {
+            $param = json_decode($param);
             $param = http_build_query($param);
         }
-        $result = [];
-        if($url) {
-            $ch = curl_init();
-            $timeout = 30;
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            if ($method == 'POST' || $method == 'post') {
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        if ($header && !is_array($header)) {
+            $header = json_decode($header);
+        }
+        if ($url) {
+            try {
+                $ch = curl_init();
+                $timeout = 30;
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                if ($method == 'POST' || $method == 'post') {
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                }
+                if($header){
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+                }
+                if ($param) {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+                }
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+                $result = curl_exec($ch);
+                curl_close($ch);
+                return $result;
+            } catch(\Exception $ex) {
+                $response['message'] .= $ex->getMessage();
+                return response()->json($response);
             }
-            if($header){
-                curl_setopt($ch, CURLOPT_HTTPHEADER, [$header]);
-            }
-            if ($param != '') {
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
-            }
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-            $result = curl_exec($ch);
-            curl_close($ch);
 
+        } else {
+            $response['message'] .= ' Url is required.';
+            return response()->json($response);
         }
-        $response = [
-            'status' => 'fail',
-            'message' => 'Request failed.'
-        ];
-        if (count($result) > 0) {
-            $response = $result;
-        }
-        return response()->json($response);
+
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 Route::group(['prefix' => 'proxy', 'namespace' => '\Megaads\TransitStation\Controllers'], function(){
-    Route::any('transferring', [
-        'as' => 'proxy::transferring', 'uses' => 'TransformerController@transferring'
+    Route::post('transferring/request', [
+        'as' => 'proxy::transferring::request', 'uses' => 'TransformerController@transferringRequest'
     ]);
 });
