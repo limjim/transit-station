1c12c128fc69804535be380e1ed87616cc646244
Kim Tung <nguyenkim08@gmail.com>
2018-10-15 14:41:07 +0700

    add options for request

--- src/Controllers/TransformerController.php
+++ src/Controllers/TransformerController.php
@@ -7,28 +7,42 @@ use Megaads\TransitStation\Controllers\Controller;
 
 class TransformerController extends Controller {
 
-    public function sendRequest(Request $request) {
+    public function transferring(Request $request) {
         header('Content-Type: application/json');
         $url = $request->has('url') ? $request->input('url') : '';
         $header = $request->has('header') ? $request->input('header') : '';
+        $method = $request->has('method') ? $request->input('method') : 'GET';
+        $param = $request->has('param') ? $request->input('param'): '';
+        if ($param != '' && is_array($param)) {
+            $param = http_build_query($param);
+        }
         $result = [];
         if($url) {
             $ch = curl_init();
             $timeout = 30;
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            if ($method == 'POST' || $method == 'post') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            }
             if($header){
                 curl_setopt($ch, CURLOPT_HTTPHEADER, [$header]);
+            }
+            if ($param != '') {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
             }
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
             $result = curl_exec($ch);
             curl_close($ch);
 
         }
-        $data = [
-            'status' => 'successful',
-            'result' => $result,
+        $response = [
+            'status' => 'fail',
+            'message' => 'Request failed.'
         ];
-        return response()->json($data);
+        if (count($result) > 0) {
+            $response = $result;
+        }
+        return response()->json($response);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
-Route::group(['prefix' => 'service', 'namespace' => '\Megaads\TransitStation\Controllers'], function(){
-    Route::post('site/send-request', [
-        'as' => 'tranformer::site::send::request', 'uses' => 'TransformerController@sendRequest'
+Route::group(['prefix' => 'proxy', 'namespace' => '\Megaads\TransitStation\Controllers'], function(){
+    Route::any('transferring', [
+        'as' => 'proxy::transferring', 'uses' => 'TransformerController@transferring'
     ]);
 });
